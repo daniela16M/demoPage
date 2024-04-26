970294317e0e147f05131c4aff7a16d4820ccba0
daniela16M <125633049+daniela16M@users.noreply.github.com>
2024-04-26 08:50:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ window.addEventListener('scroll', ()=>{
         contentNav.style.backgroundColor = 'transparent'
     }
 })
+
+
+// Obtener elementos del DOM
+const modal = document.getElementById('contactModal');
+const openLink = document.getElementById('openModalLink');
+const closeBtn = modal.querySelector('.close');
+
+// Función para abrir el modal
+function openModal(event) {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    modal.style.display = 'block';
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Evento de clic para abrir el modal al hacer clic en el enlace
+openLink.addEventListener('click', openModal);
+
+// Evento de clic para cerrar el modal al hacer clic en el botón de cerrar
+closeBtn.addEventListener('click', closeModal);
+
+// Evento de clic para cerrar el modal al hacer clic fuera del contenido del modal
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
